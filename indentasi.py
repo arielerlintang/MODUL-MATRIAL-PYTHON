bfb98e97a46c3ca55bac1363af7a7bb6c25bd32f
Ariel Erlintang <115611813+arielerlintang@users.noreply.github.com>
2026-03-25 11:59:57 +0700

    Create indentasi.py
    indentasi

---
+++ indentasi.py
@@ -0,0 +1,16 @@
+7. INDENTASI
+
+Python menggunakan indentasi untuk menandai blok program.
+
+if True:
+    print("Baris ini masuk ke dalam if")
+    print("Masih di dalam if")
+
+print("Baris ini di luar if")
+
+Kalau indentasi salah, program akan error.
+
+Contoh salah:
+
+if True:
+print("Hello")
