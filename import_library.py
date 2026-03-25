aef5028f57255528e39149360bd8e7035761e10c
Ariel Erlintang <115611813+arielerlintang@users.noreply.github.com>
2026-03-25 12:01:07 +0700

    Create import_library.py

---
+++ import_library.py
@@ -0,0 +1,11 @@
+9. IMPORT LIBRARY
+9.1 Import library bawaan
+import math
+
+print(math.sqrt(25))
+9.2 Import pandas
+import pandas as pd
+Install pandas
+pip install pandas
+Install openpyxl
+pip install openpyxl
